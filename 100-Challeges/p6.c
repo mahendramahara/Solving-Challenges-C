d188448384c8f7b3c7f0ea8ea46329295eb98905
mahendramahara <mahendramahara369@gmail.com>
2022-07-04 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p6.c
@@ -0,0 +1,14 @@
+/* Functions */
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = add(5, 3);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
