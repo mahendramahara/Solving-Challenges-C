ea1c4bf0b045c856d15ba8e1654953006f0e2156
mahendramahara <mahendramahara369@gmail.com>
2022-07-08 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p9.c
@@ -0,0 +1,11 @@
+/* Strings */
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hello, world!";
+
+    printf("String: %s\n", str);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
