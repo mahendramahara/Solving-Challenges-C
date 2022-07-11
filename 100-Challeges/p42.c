559054241d82803f5251614b871ece27a27ee4e6
mahendramahara <mahendramahara369@gmail.com>
2022-07-11 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p42.c
@@ -0,0 +1,26 @@
+/*
+Title: Modular Programming
+Author: Mahendra Mahara
+Date: 2023
+*/
+
+#include <stdio.h>
+
+// Function prototypes
+void greet();
+void farewell();
+
+int main() {
+    greet();
+    farewell();
+    return 0;
+}
+
+// Function definitions
+void greet() {
+    printf("Hello! Welcome to Modular Programming.\n");
+}
+
+void farewell() {
+    printf("Goodbye! Thank you for using Modular Programming.\n");
+}
