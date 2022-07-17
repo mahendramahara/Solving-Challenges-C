df12de45d9afe273276207fe8185bf861cd8db65
mahendramahara <mahendramahara369@gmail.com>
2022-07-17 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p7.c
@@ -0,0 +1,16 @@
+/* Arrays */
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
