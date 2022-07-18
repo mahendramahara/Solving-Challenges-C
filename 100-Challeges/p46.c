6f3c66fec5201ad730e2851a778c429049c27f2b
mahendramahara <mahendramahara369@gmail.com>
2022-07-18 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p46.c
@@ -0,0 +1,18 @@
+/*
+Title: Demonstration of Debugging Techniques
+*/
+
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 10; ++i) {
+        sum += i;
+    }
+    
+    printf("Sum of first 10 natural numbers: %d\n", sum);
+    
+    return 0;
+}
+
+/* Copyright (c) 2023 Mahendra Mahara */
