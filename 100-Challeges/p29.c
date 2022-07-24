54f099a5fc2c45e9a46fcc9ab055c176c0e26417
mahendramahara <mahendramahara369@gmail.com>
2022-07-24 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p29.c
@@ -0,0 +1,29 @@
+/* Trees */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Binary tree created successfully!\n");
+
+    return 0;
+}
