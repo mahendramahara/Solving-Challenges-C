db41a7fe048d4972b94340ef1b73b321e584036c
mahendramahara <mahendramahara369@gmail.com>
2022-07-28 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p82.c
@@ -0,0 +1,72 @@
+/* Program 82: Networking: Write a simple client-server application using sockets. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[MAX_BUFFER_SIZE] = {0};
+    const char *response = "Hello from server";
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attach socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the specified port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive data from client
+    read(new_socket, buffer, MAX_BUFFER_SIZE);
+    printf("Client: %s\n", buffer);
+
+    // Send response to client
+    send(new_socket, response, strlen(response), 0);
+    printf("Response sent to client.\n");
+
+    // Close socket
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
+
+/* 
+   Copyright (c) 2023
+   Mahendra Mahara
+*/
