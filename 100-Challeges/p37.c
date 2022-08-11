3b25e1d61e330b237d625e11f63cd051b10eb3e3
mahendramahara <mahendramahara369@gmail.com>
2022-08-11 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p37.c
@@ -0,0 +1,13 @@
+/* Socket Programming */
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        return 1;
+    }
+    printf("Socket created successfully\n");
+    return 0;
+}
