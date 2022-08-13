739cc2e33e5778b6cc185fd10d2450eea675620d
mahendramahara <mahendramahara369@gmail.com>
2022-08-13 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p4.c
@@ -0,0 +1,14 @@
+/* Operators */
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
