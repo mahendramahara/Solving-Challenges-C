5ee726aa82d94994371e7b705f5d787c379577d8
mahendramahara <mahendramahara369@gmail.com>
2022-08-27 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p31.c
@@ -0,0 +1,36 @@
+/* Sorting Algorithms */
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array before sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    printf("Array after sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
