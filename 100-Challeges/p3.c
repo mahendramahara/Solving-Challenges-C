6a40024b9d7fbaf6c87f81210e302093578999a2
mahendramahara <mahendramahara369@gmail.com>
2022-09-15 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p3.c
@@ -0,0 +1,13 @@
+/* Variables */
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
