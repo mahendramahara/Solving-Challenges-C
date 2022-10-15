c71fe6f8f54d1443cff355061d9b164eb046a5be
mahendramahara <mahendramahara369@gmail.com>
2022-10-15 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p22.c
@@ -0,0 +1,14 @@
+/* String Functions (string.h) */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "Hello";
+    char str2[] = "World";
+
+    strcat(str1, str2);
+
+    printf("Concatenated string: %s\n", str1);
+
+    return 0;
+}
