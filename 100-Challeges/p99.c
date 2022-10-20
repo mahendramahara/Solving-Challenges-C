f7cd2048b7debeab922fd1c63449180881abf28a
mahendramahara <mahendramahara369@gmail.com>
2022-10-20 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p99.c
@@ -0,0 +1,10 @@
+// Commonly used library functions for text rendering in C GUI applications
+
+
+
+    // Some commonly used functions for text rendering in C GUI applications include:
+    // - outtextxy(): Renders text at specified coordinates
+    // - settextstyle(): Sets the font size and style
+    // - setcolor(): Sets the text color
+    // - setbkcolor(): Sets the background color for text
+    // - settextjustify(): Sets the horizontal and vertical justification of text
