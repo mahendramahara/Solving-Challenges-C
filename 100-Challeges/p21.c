a59008110e941563ab6fbe81f02c8b13f2b46401
mahendramahara <mahendramahara369@gmail.com>
2022-10-29 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p21.c
@@ -0,0 +1,11 @@
+/* Math Functions (math.h) */
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double num = 4.0;
+
+    printf("Square root of %lf: %lf\n", num, sqrt(num));
+
+    return 0;
+}
