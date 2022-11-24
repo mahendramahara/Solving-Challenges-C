a6ae18653a0a8c646c770588eecf1d83cae83990
mahendramahara <mahendramahara369@gmail.com>
2022-11-24 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p5.c
@@ -0,0 +1,17 @@
+/* Control Flow */
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+
+    if (num > 0) {
+        printf("Number is positive.\n");
+    } else if (num < 0) {
+        printf("Number is negative.\n");
+    } else {
+        printf("Number is zero.\n");
+    }
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
