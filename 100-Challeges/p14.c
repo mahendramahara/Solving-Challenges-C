b1b8a971affd912d6e7ab2034f30e42b6316b430
mahendramahara <mahendramahara369@gmail.com>
2022-12-08 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p14.c
@@ -0,0 +1,16 @@
+/* Preprocessor Directives */
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    float radius = 5.0;
+    float area;
+
+    area = PI * radius * radius;
+
+    printf("Area of circle: %f\n", area);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
