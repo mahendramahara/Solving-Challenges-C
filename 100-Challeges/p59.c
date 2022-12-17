2ec9594e1bbf149c34a1bb23c5890228efa3b614
mahendramahara <mahendramahara369@gmail.com>
2022-12-17 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p59.c
@@ -0,0 +1,25 @@
+/* Program 59: Write a function to calculate the factorial of a number. */
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
+
+/* 
+   Copyright (c) 2023
+   Mahendra Mahara
+*/
