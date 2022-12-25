ae0b3652ebd36ad09d1bcd3f25bd387581a39cfd
mahendramahara <mahendramahara369@gmail.com>
2022-12-25 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p50.c
@@ -0,0 +1,10 @@
+/* Program 50: Demonstration of Basic Input and Output Operations in C */
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
+    return 0;
+}
