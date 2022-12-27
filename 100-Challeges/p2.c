b88ff6dd8f408ba1b8e52dc1f881a82aba4de806
mahendramahara <mahendramahara369@gmail.com>
2022-12-27 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ 100-Challeges/p2.c
@@ -0,0 +1,15 @@
+/* Data Types */
+#include <stdio.h>
+
+int main() {
+    int integerVar = 10;
+    float floatVar = 3.14;
+    char charVar = 'A';
+
+    printf("Integer: %d\n", integerVar);
+    printf("Float: %f\n", floatVar);
+    printf("Character: %c\n", charVar);
+
+    return 0;
+}
+/* Copyright (c) 2023 Mahendra Mahara */
