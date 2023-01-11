61951d2a408c116359854232c3a38faf0d339245
mahendramahara <mahendramahara369@gmail.com>
2023-01-11 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ C-Programs/Month-1/1_p5_20.c
@@ -0,0 +1,14 @@
+// Write a  C Program to Swap Numbers without Using Temporary Variable
+
+#include <stdio.h>
+
+int main(){
+	printf("\n ======================= SWAP 2 NUMBERS WITHOUT USING TEMPORARY VARIABLES ====================== \n"); 
+	float a, b;
+	printf("\n Enter the first number [X] : "); 
+	scanf("%f", &a);
+	printf("\n Enter the second number [Y] : "); 
+	scanf("%f", &b);
+	printf("\n First Number [X] = %.3f\n Second Number [Y] = %.3f\n", b, a); 
+	return 0;
+}
