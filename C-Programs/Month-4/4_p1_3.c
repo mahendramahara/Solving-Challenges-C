5133f8570253212562becda526b92b4d332053fd
mahendramahara <mahendramahara369@gmail.com>
2023-03-07 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ C-Programs/Month-4/4_p1_3.c
@@ -0,0 +1,8 @@
+//WAP in C to for the following logical expression and print the results;
+#include <stdio.h>
+
+int main(){
+    int i,j,k,l;
+    i++ && j++ && k++ || l++;
+    printf("i = %d\nj = %d\nk = %d\nl = %d",i,j,k,l);
+}
