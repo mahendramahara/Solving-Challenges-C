1e8f4442cde03eff83042247a5ce9f9039b264d0
mahendramahara <mahendramahara369@gmail.com>
2023-03-18 07:00:00 +0545

    100+ C-Programming Challenges

---
+++ C-Programs/Month-4/4_p2_6.c
@@ -0,0 +1,10 @@
+//     6. Write  a C program to Find the output of the following expression 
+//     5||2|1
+
+#include <stdio.h>
+
+int main(){
+	printf("\n ======================= OUTPUT OF THE EXPRESSION : 5||2|1 ====================== \n"); 
+	printf("\n %d", 5||2|1);
+	return 0;
+}
